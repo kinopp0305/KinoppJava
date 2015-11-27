ccbadbd4a4272e2f3ba4d97789529671c6b1d161
木下英樹 <kinopp@myhomene.jp>
2015-11-27 10:29:01 +0900

    サンプル追加

--- src/date/JapaneseDateSample.java
+++ src/date/JapaneseDateSample.java
@@ -1,8 +1,11 @@
 package date;
 
+import java.time.chrono.JapaneseChronology;
 import java.time.chrono.JapaneseDate;
+import static java.time.chrono.JapaneseDate.now;
 import java.time.chrono.JapaneseEra;
 import java.time.format.DateTimeFormatter;
+import static java.time.temporal.ChronoField.*;
 
 /**
  * JapaneseDateサンプル
@@ -14,12 +17,19 @@ public class JapaneseDateSample {
         System.out.println(date1);  //=> Japanese Showa 48-05-23
         System.out.println(date1.getChronology());  //=> Japanese
         System.out.println(date1.getEra());  //=> Showa
+        System.out.println(date1.get(YEAR_OF_ERA));  //=> 48
+        System.out.println(date1.get(YEAR));  //=> 1973
+        System.out.println(date1.get(ERA));  //=> 1
 
         // 和暦を設定
         JapaneseDate date2 = JapaneseDate.of(JapaneseEra.HEISEI, 18, 4, 24);
         System.out.println(date2);  //=> Japanese Heisei 18-04-24
         System.out.println(date2.format(DateTimeFormatter.ISO_DATE));  //=> 2006-04-24
         System.out.println(date2.format(DateTimeFormatter.BASIC_ISO_DATE));  //=> 20060424
+        
+        // 現在日付を設定
+        JapaneseDate date3 = JapaneseDate.now();
+        System.out.println(date3);  //=> Japanese Heisei 27-11-27
     }
 
 }
